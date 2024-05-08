bc08b3ab5b46c69cfc86a9321fb657cd79726c28
Kebron Abiy <kebronabiy589@gmail.com>
2024-05-08 17:24:48 +0300

    main and sidebar navigation added.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,6 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
 
 export default function RootLayout({
@@ -5,10 +8,24 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {firstName: "Roman", lastName: "Reigns"}
   return (
-    <main>
-        SIDEBAR
-        {children}
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn}/>
+        <div className="flex flex-col size-full">
+          <div className="root-layout">
+            <Image
+              src="/icons/logo.svg"
+              width={30}
+              height={30}
+              alt="logo"
+            />
+            <div>
+              <MobileNav user={loggedIn}/>
+            </div>
+          </div>
+          {children}
+        </div>
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,27 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 function Home() {
+    const loggedIn = {firstName: "Roman"}
   return (
-    <div>Home</div>
+    <section className="home">
+        <div className="home-content">
+            <header className="home-header">
+                <HeaderBox 
+                    type="greeting"
+                    title="Welcome"
+                    user={loggedIn?.firstName || "Guest"}
+                    subtext="Access and Manage your accounts and transactions effeciently."
+                />
+                <TotalBalanceBox
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1500}
+                />
+            </header>
+        </div>
+    </section>
   )
 }
 
